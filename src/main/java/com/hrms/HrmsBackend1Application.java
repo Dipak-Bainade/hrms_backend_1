c56594ba25221f87ddc15dc7015e1e3a8083919f
Dipak-Bainade <dipakbainade121@gmail.com>
2026-07-05 14:52:20 +0530

    initial commit

---
+++ src/main/java/com/hrms/HrmsBackend1Application.java
@@ -0,0 +1,13 @@
+package com.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsBackend1Application.class, args);
+	}
+
+}
